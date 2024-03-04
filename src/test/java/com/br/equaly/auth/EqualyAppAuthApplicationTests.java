2c841b0e78be1d70fde8125072b8a7262b857e35
RafaelRenaud <renaudrafa@hotmail.com>
2024-03-04 02:22:13 -0300

    conf: Project Template Configuration

---
+++ src/test/java/com/br/equaly/auth/EqualyAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.equaly.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualyAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
